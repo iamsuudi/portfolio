c27afbeae7099eecb90615cfe88dec6fec558dc0
Abdulfetah <suudiabdulfetah@gmail.com>
2025-05-11 19:50:59 +0300

    Update metadata and Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,48 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
-    title: "Abdulfetah Suudi - Portfolio",
-    description: "Software Engineer",
+    title: {
+        default: siteConfig.name,
+        template: `%s - ${siteConfig.name}`,
+    },
+    metadataBase: new URL("https://suudi.abdulfetah.com"),
+    description: siteConfig.description,
+    keywords: ["Software", "Engineer", "Full-Stack", "Developer"],
+    authors: [
+        {
+            name: "Abdulfetah Suudi",
+            url: "https://suudi.abdulfetah.com",
+        },
+    ],
+    creator: "Abdulfetah",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://suudi.abdulfetah.com",
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+        images: [
+            {
+                url: "https://suudi.abdulfetah.com/og.png",
+                width: 1200,
+                height: 630,
+                alt: siteConfig.name,
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: siteConfig.name,
+        description: siteConfig.description,
+        images: ["https://suudi.abdulfetah.com/og.png"],
+        creator: "@iamsuudi",
+    },
+    manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
@@ -24,6 +62,7 @@ export default function RootLayout({
                 >
                     {children}
                 </ThemeProvider>
+                <Analytics />
             </body>
         </html>
     );
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Abdulfetah Suudi",
+    url: "https://suudi.abdulfetah.com",
+    ogImage: "https://suudi.abdulfetah.com/og.png",
+    description:
+        "A full stack software engineer with end-to-end expertise in building, architecting, and maintaining scalable web applications. I design clean and modular system architectures and manage infrastructure for seamless deployment and maintenance.",
+    links: {
+        twitter: "https://twitter.com/iamsuudi",
+        github: "https://github.com/iamsuudi",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
